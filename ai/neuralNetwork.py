ed9228613e8bf5cd0807dce540fb27a90d473f8e
maielln@sunyit.edu <maielln@sunyit.edu>
2018-11-27 02:43:20 -0500

    Put in neural network with testing and predicting

--- ai/neuralNetwork.py
+++ ai/neuralNetwork.py
@@ -1,49 +1,49 @@
-#importing TensorFlow and Keras
 import tensorflow as tf
-import stockDataFormatter.py #can pull from a main instead
 from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense
+import os
 
-#Libraries that may be useful
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
+#utilizing a linear model
+model = Sequential()
 
-#designing to work with stockDataFormatter.py
-#tf.Variable is used in the graph for tensorflow
-#tf.random_normal outputs random values from a normal distribution
+#creating model with 2 hidden layers
+#bias initialization is automatically zero
+model.add(Dense(8, input_dim = 4, activation = 'relu'))
+model.add(Dense(8, activation = 'relu'))
+model.add(Dense(1, activation = 'sigmoid'))
 
-#some parameters for now, change later
-learning_rate = 0.1
-num_steps = 500
-batch_size = 128
-display_step = 100
+#binary classification
+model.compile(optimizer = 'rsmprop',
+              loss = 'binary_crossentropy',
+              metrics = ['accuracy'])
 
-#NN parameters
-num_input = len(stockDataFormatter.formatRawData)
-n_hiddenNeurons_1 = 256
-n_hiddenNeurons_2 = 256 #random numbers for now
+def NN (dataIn):
+    #checking if file exists
+    exists = os.path.isfile('model.hdf5')
+    if exists:
+        model.load_weights('model.hdf5')
+        print("Loading model weights from disk.")
+    else:
+        print("Weights not found, creating new model.")
+    
+    train(dataIn)
+    print(predict(dataIn))
+    print("Session complete, saving model weights to disk.")
+    #saving the weights
+    model.save_weights('model.hdf5')
+    print("Successfully saved model weights to disk.")
 
-#tf Graph input - placeholders
-X = tf.placeholder(tf.float32, [None, 784])
-Y = tf.placeholder(tf.float32, [None, 784])
+def train(dataIn):
+    print("Beginning training.")
+    i = 0
+    for i in range(0,8):
+        model.fit(dataIn[i], nb_epoch = 500, verbose = 2)
+    print("Training complete... predicting.")
 
-#initializing weights
-weights = {
-    'h1': tf.Variable(tf.random_normal[num_input, n_hiddenNeurons_1]),
-    'h2': tf.Variable(tf.random_normal[n_hiddenNeurons_1, n_hiddenNeurons_2]),
-    'out': tf.Variable(tf.random_normal[n_hiddenNeurons_2, 10])
-}
-
-biases = {
-    'b1': tf.Variable(tf.random_normal([n_hiddenNeurons_1])),
-    'b2': tf.Variable(tf.random_normal([n_hiddenNeurons_2])),
-    'out': tf.Variable(tf.random_normal([10]))
-}
-
-#dataset holds: date, raw stock data
-#2 layer neural network for now
-def neural_net():
-#code for neural network here
-
-def train_sess():
-#code for training sessions
+def predict(dataIn):
+    i = 9
+    results = []
+    for i in range (9,10):
+        results.append(model.predict(dataIn).round())
+    return results
