854a57a728fbed6a26ed1c46cb9cbc62d0f43bc9
maielln@sunyit.edu <maielln@sunyit.edu>
2018-12-01 00:35:35 -0500

    Integrated Neural Network with Stock Data

--- ai/neuralNetwork.py
+++ ai/neuralNetwork.py
@@ -2,48 +2,39 @@ import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense
+#from keras.optimizers import SGD
 import os
 
 #utilizing a linear model
 model = Sequential()
-
-#creating model with 2 hidden layers
 #bias initialization is automatically zero
-model.add(Dense(8, input_dim = 4, activation = 'relu'))
-model.add(Dense(8, activation = 'relu'))
+model.add(Dense(256, input_dim = 4, activation = 'relu'))
+model.add(Dense(256, activation = 'relu'))
 model.add(Dense(1, activation = 'sigmoid'))
 
-#binary classification
-model.compile(optimizer = 'rsmprop',
-              loss = 'binary_crossentropy',
-              metrics = ['accuracy'])
+#opt = SGD(lr=0.01)
 
-def NN (dataIn):
+model.compile(optimizer = 'adam',
+              loss = 'mean_squared_error')
+              #metrics = ['accuracy']) - removed for regression
+
+def NN (saveFile=None):
+    if saveFile == None: 
+        return
     #checking if file exists
-    exists = os.path.isfile('model.hdf5')
+    exists = os.path.isfile(saveFile)
     if exists:
-        model.load_weights('model.hdf5')
-        print("Loading model weights from disk.")
+        model.load_weights(saveFile)
+        print("Loading model weights from disk. " + saveFile)
     else:
         print("Weights not found, creating new model.")
-    
-    train(dataIn)
-    print(predict(dataIn))
-    print("Session complete, saving model weights to disk.")
-    #saving the weights
-    model.save_weights('model.hdf5')
-    print("Successfully saved model weights to disk.")
 
-def train(dataIn):
+def train(dataIn, dataOut):
     print("Beginning training.")
-    i = 0
-    for i in range(0,8):
-        model.fit(dataIn[i], nb_epoch = 500, verbose = 2)
+    mc = keras.callbacks.ModelCheckpoint('weights/weights{epoch:08d}.h5', save_weights_only=True, period=500)
+    model.fit(x=dataIn, y=dataOut, epochs = 5000000000, verbose = 2, callbacks=[mc])
+    model.save_weights('model.h5')
     print("Training complete... predicting.")
 
 def predict(dataIn):
-    i = 9
-    results = []
-    for i in range (9,10):
-        results.append(model.predict(dataIn).round())
-    return results
+    return model.predict(dataIn)
